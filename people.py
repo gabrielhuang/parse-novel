0444b189d52fc820dea502c1966a4c409a717566
gabi <gabi.xiaohuang@gmail.com>
2015-05-12 02:43:21 -0400

    [ADD] Alternative method to POS tagging for stopword detection

--- people.py
+++ people.py
@@ -5,9 +5,10 @@ Created on Thu May 07 10:52:57 2015
 @author: gabi
 """
 
+import time
+import sys
 import nltk
 import re
-import numpy as np
 from scipy.sparse.csgraph import connected_components
 from scipy.sparse import csr_matrix
 from nltk.corpus import wordnet as wn
@@ -18,16 +19,22 @@ male_names = open('data/dist.male.first.txt').read().lower().split()[::4]
 female_names = open('data/dist.female.first.txt').read().lower().split()[::4]
 last_names = open('data/dist.all.last.txt').read().lower().split()[::4]
 person_synset = wn.synset('person.n.01')
+object_synset = wn.synset('object.n.01') # includes location
+# read stopwords
+stopwords_file = open('data/stopwords.csv')
+stopwords_file.readline() # read header
+stopwords = stopwords_file.read().split()
+stopwords_file.close()
 
 
 def is_person_wordnet(word):
     '''
     Returns if WordNet knows a person named 'word'
-    Rules:
-    1) If not synset corresponds, it's a person
+    Rules (by precedence):
+    1) If no synset corresponds, it's a person
     2) If synsets are found but don't contain nouns, it's not a person
-    3) If synsets are found but all noun synsets are instances it's a person
-    4) If at least one of the synsets inherits from person.n.01 then it's a person
+    3) If at least one of the synsets inherits from a location then it's not a person
+    4) If at least one of the synsets inherits from person.n.01 then it's a person    
     '''
     synsets = wn.synsets(word)
     if not synsets: # Rule 1
@@ -35,14 +42,15 @@ def is_person_wordnet(word):
     if all(synset.pos()!='n' for synset in synsets): # Rule 2
         return False
     # At this point there is at least one noun
-    instances = []
+    hyper = set()
     for synset in synsets:
         if synset.pos() == 'n':
-            hyper = set([i for i in synset.closure(lambda s:s.hypernyms())])
-            if person_synset in hyper: # Rule 4
-                return True
-            instances.append(not hyper)
-    return all(instances) # Rule 3
+            hyper = hyper | set([i for i in synset.closure(lambda s:s.hypernyms()+s.instance_hypernyms())])
+    if object_synset in hyper: # Rule 3
+        return False
+    if person_synset in hyper: # Rule 4
+        return True
+    return False
 
 
 def is_person_census(word):
@@ -58,7 +66,8 @@ def is_person(words):
     '''
     if len(words)>1 and words[0] in titles:
         return True
-    return all(is_person_census(word.lower()) or is_person_wordnet(word.lower()) for word in words)
+    return all(word.lower() not in stopwords for word in words) and \
+        all(is_person_census(word.lower()) or is_person_wordnet(word.lower()) for word in words)
 
 
 def extract_ne(txt):
@@ -72,13 +81,49 @@ def extract_ne(txt):
     return ne
 
 
+#Punctuation that capitalizes the next word
+shift_punct = set(('.','!','?'))
+is_uppercase = re.compile(r'[A-Z]+')
+
+def extract_no_postag(txt, ignore_after_punct=False):
+    '''
+    Alternative Step 1.
+    Same as extract_ne without POS tagging
+    
+    Parameters
+    :ignore_after_punct: if true then ignore capitalized words after punctuation
+    
+    Returns
+    list of names instead of nltk trees
+    '''
+    words = nltk.word_tokenize(txt)
+    names = []
+    previous = None
+    for i, word in enumerate(words):
+        if not ignore_after_punct or (i==0)\
+        or( words[i-1] not in shift_punct) or word in titles:
+            if word and is_uppercase.match(word[0]):
+                if previous is not None and previous==i-1:                
+                    names[-1] += ' ' + word
+                else:
+                    names.append(word)
+                previous = i
+    return names
+
+
 def destructure_ne(named_entity):
     parts = [word for word,tag in named_entity.leaves()]
     key = ' '.join(parts)
     return key, parts
 
 
-def accumulate_ch(named_entities):
+def destructure_no_postag(named_entity):
+    parts = named_entity.split()
+    key = named_entity
+    return key, parts
+    
+
+def accumulate_ch(named_entities, destructure_ne=destructure_ne):
     '''
     Step 2. Remove duplicates and return unfiltered list of characters (might be redundant)
     '''
@@ -132,7 +177,7 @@ def merge_ch(characters):
     return synsets
 
 
-def detect_ch(txt, final_characters, verbose=False):
+def detect_ch(txt, final_characters, extract_ne=extract_ne, destructure_ne=destructure_ne, verbose=False):
     '''
     Step 5. (Prediction) Given trained final characters, detect them in a given (new) paragraph
     '''
@@ -169,8 +214,8 @@ class People:
         ch1 = accumulate_ch(named_entities)
         ch2 = filter_ch(ch1, min_count=min_count)
         self.final_characters = merge_ch(ch2)
-    def predict(self, text):
-        detected_ch = detect_ch(text, self.final_characters)
+    def predict(self, text, **kwargs):
+        detected_ch = detect_ch(text, self.final_characters, **kwargs)
         return detected_ch
     def get(self, index):
         '''
@@ -182,20 +227,45 @@ class People:
         Return number of persons learned during training
         '''
         return len(self.final_characters)
+
+
+class PeopleNoPos(People):
+    def __init__(self, train_set=None, **kwargs):
+        '''
+        This is like People but uses a simple and fast caps-based heuristic        
+        instead of part of speech tagging.
+        Will train model if train_set is not None
+        '''
+        People.__init__(self, train_set, **kwargs)
+    def train(self, text, min_count=1):
+        named_entities = extract_no_postag(text, ignore_after_punct=True)
+        ch1 = accumulate_ch(named_entities, destructure_ne=destructure_no_postag)
+        ch2 = filter_ch(ch1, min_count=min_count)
+        self.final_characters = merge_ch(ch2)
+    def predict(self, text, **kwargs):
+        detected_ch = detect_ch(text, self.final_characters, 
+                                extract_ne=extract_no_postag, destructure_ne=destructure_no_postag, **kwargs)
+        return detected_ch
+      
+        
         
 #%% Test
 if __name__=='__main__':
-    txt = open('data/gatsby.txt').read()[10000:20000]
+    txt = open('data/gatsby.txt').read()[10000:40000]
+    txt2 = open('data/gatsby.txt').read()[50000:80000]
     
-    print '\n\nTraining Characters'
-    named_entities = extract_ne(txt)
-    ch1 = accumulate_ch(named_entities)
-    ch2 = filter_ch(ch1)
-    final_characters = merge_ch(ch2)
-    print final_characters
-
-    #%%
-    print '\n\nDetecting Characters'
-    txt2 = open('data/gatsby.txt').read()[20000:30000]
-    detected_ch = detect_ch(txt2, final_characters, verbose=True)
-    print detected_ch
+    for name, PeopleType in [('PeopleNoPos',PeopleNoPos), ('People',People)]:
+        last_time = t = time.time()
+        print '\nModel name {}'.format(name)
+        print 'Training Characters'
+        ppl = PeopleType(txt)
+        print 'Found \n{}\n'.format(ppl.final_characters)
+        
+        #%%
+        print 'Detecting Characters'
+        
+        detected_ch = ppl.predict(txt2, verbose=False)
+        characters = map(ppl.get, detected_ch)
+        print characters
+        print 'Total time {} seconds'.format(time.time() - last_time)
+        sys.stdout.flush()
